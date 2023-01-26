1bb73b4845189cbf715c0a19b6b14a2ac760b5a1
Daniel Rodriguez <dbenjaminrdz@gmail.com>
2023-01-26 14:15:18 -0800

    Change exported classes for package

--- lib/taboo.dart
+++ lib/taboo.dart
@@ -1,1 +1,12 @@
 library taboo;
+
+export 'package:taboo/pages/taboo_page.dart';
+export 'package:taboo/service/tab_service.dart';
+
+export 'package:taboo/tabs/document_tab.dart';
+export 'package:taboo/tabs/static_tab.dart';
+export 'package:taboo/tabs/taboo_tab.dart';
+
+export 'package:taboo/views/bar/taboo_bar.dart';
+export 'package:taboo/views/tab/taboo_tab_view.dart';
+export 'package:taboo/views/page/taboo_page_view.dart';
